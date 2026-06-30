08185ffb99c0e1b122c38d1a4315687bbb5a5803
BrunnoRosa <bruno.r.silva7@ba.estudante.senai.br>
2026-06-29 21:32:36 -0300

    implementação do clienteService.js no frontEnd

--- tech_nexus/src/service/clientesService.js
+++ tech_nexus/src/service/clientesService.js
@@ -1,15 +1,40 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'http://localhost:8080/api/clientes'
 })
 
 export async function listarClientes() {
-  const response = await api.get('/clientes')
-  return response.data
+  try{
+    const response = await fetch (api);
+
+    if (!response.ok) {
+      throw new Error('Erro ao buscar clientes no servidor.');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Erro ao listar clientes:', error);
+    throw error;
+  }
 }
 
-export async function cadastrarCliente(cliente) {
-  const response = await api.post('/clientes', cliente)
-  return response.data
+export async function cadastrarCliente(dadosFormulario) {
+  try {
+    const response = await fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dadosFormulario),
+    });
+
+    if (!response.ok) {
+      throw new Error('Erro ao cadastrar cliente no servidor.');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Erro ao cadastrar cliente:', error);
+    throw error;
+  }
 }
